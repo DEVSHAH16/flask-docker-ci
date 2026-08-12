fe1622c4d50e312af25398fbfc30816ada7728b0
DEVSHAH16 <your_email@example.com>
2026-08-12 22:33:56 +0530

    Added  flat fee

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def calculate_total(price, tax):
-    total = price - 10 + (price * tax)  # <-- Changed this line
+    total = price + 5 + (price * tax)  # <-- Changed this line differently
     return total
 
 print(calculate_total(100, 0.05))
